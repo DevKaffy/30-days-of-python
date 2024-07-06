c54714586a52eef802fcce00f19367adc6fe4587
DevKaffy <kafilatadewumi61@gmail.com>
2024-07-06 15:09:48 +0100

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,44 @@
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name: ', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+
+
+print(3 + 4)
+print(3 * 4)
+print(4 - 3)
+print(4 % 3)
+print(4 / 3)
+print(3 ** 4)
+print(4 // 3)
+
+name = "Kafilat"
+surname = "Adewumi"
+country = "Nigeria"
+feeling = "I am enjoying 30 days of Python"
+print(name)
+print(surname)
+print(country)
+print(feeling)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(('Asabeneh', 'Python', 'Finland')))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(surname))
+print(type(country))
+print(type(
+    {
+        "surname":"Adewumi",
+        "name": "Kafilat",
+        "age": 13
+    }
+))
+print(type({3, 4, 6, 7}))
---
+++ day_2/variables.py
@@ -0,0 +1,52 @@
+# Day 2: 30 Days of python programming
+first_name = "Kafilat"
+last_name = "Adewumi"
+full_name = last_name + first_name
+country = "Nigeria"
+city = "Ilorin"
+age = 28
+year = 1996
+is_married = True
+is_true = True
+is_light_on = False
+school, system, level = "University of Ilorin", "Macbook pro", "year four"
+
+
+print(school, system, level)
+print(len(first_name))
+print(len(last_name))
+print((len(first_name)) > (len(last_name)))
+print(len(full_name))
+print(type(age))
+
+
+
+a = 5
+b = 4
+# add
+print(f'{a} + {b} = {a + b}')
+# subtract
+print(f'{a} - {b} = {a - b}')
+# multiply
+print(f'{a} * {b} = {a * b}')
+# division
+print(f'{a} / {b} = {a/b}')
+# modulus
+print(f'{a} % {b} = {a % b}')
+# floor division
+print(f'{a} // {b} = {a // b}')  
+# power
+print(f'{a} ** {b} = {a ** b}')
+
+
+radius = int(input('Enter radius: '))
+area_of_circle = 3.14 * (radius ** 2)   #THr2
+print(area_of_circle)
+circum_of_circle = (2*3.14) * (radius ** 2)  #2THr2
+print(circum_of_circle)
+
+language = 'Python'
+last_index = len(language) - 1
+print(last_index)
+last_letter = language[last_index]
+print(last_letter) # n
